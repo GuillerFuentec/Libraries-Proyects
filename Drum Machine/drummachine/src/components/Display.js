ebb29e5c6e7a47cfb5ea4d92d0ad2456c840f5e2
GuillerFuentec <copelloguillermo60@gmail.com>
2024-05-15 22:23:10 -0400

    finaly finna responsive design official version

--- Drum Machine/drummachine/src/components/Display.js
+++ Drum Machine/drummachine/src/components/Display.js
@@ -1,13 +1,22 @@
-import LabelDisplay from "./components/LabelDisplay"
-import ToggleDisplay from "./components/ToggleDisplay"
+import LabelDisplay from "./components/LabelDisplay";
+import ToggleDisplay from "./components/ToggleDisplay";
+import RangeDisplay from "./components/RangeDisplay";
 
 export default function Display() {
   return (
-      <div className="Display grid grid-rows-4 grid-flow-col gap-1" >
-        <div className="toggle-box">
-          <LabelDisplay>POWER</LabelDisplay>
-          <ToggleDisplay/>
-        </div>
+    <div className="Display grid grid-rows-4 p-4 grid-flow-col gap-1">
+      <div className="toggle-box">
+        <LabelDisplay>POWER</LabelDisplay>
+        <ToggleDisplay />
       </div>
-  )
+      <div className="screen"></div>
+      <div>
+        <RangeDisplay/>
+      </div>
+      <div className="toggle-box">
+        <LabelDisplay>BANK</LabelDisplay>
+        <ToggleDisplay />
+      </div>
+    </div>
+  );
 }
--- Drum Machine/drummachine/src/components/components/ToggleDisplay.js
+++ Drum Machine/drummachine/src/components/components/ToggleDisplay.js
@@ -3,7 +3,7 @@
 export default function ToggleDisplay() {
   return (
     <>
-      <input type="checkbox" className="toggle [--tglbg:red-light] bg-black-500 hover:bg-black-700 border-black-700"/>
+      <input type="checkbox" className="toggle [--tglbg:white] opacity-20"/>
     </>
   );
 }
