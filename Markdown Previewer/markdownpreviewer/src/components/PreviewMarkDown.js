46c5cbffe9802e711fe79c49dea02f3faac12207
GuillerFuentec <copelloguillermo60@gmail.com>
2024-06-13 18:16:32 -0400

    responsive desing succeded

--- Markdown Previewer/markdownpreviewer/src/components/PreviewMarkDown.js
+++ Markdown Previewer/markdownpreviewer/src/components/PreviewMarkDown.js
@@ -1,7 +1,7 @@
-export default function PreviewMarkDown({ previewmarkdown, ...props }) {
+export default function PreviewMarkDown({ previewmarkdown }) {
   return (
     <>
-      <div className="grid my-8 w-1/2 card-p place-items-center">
+      <div className="grid my-8 w-full sm:w-1/2 card-p place-items-center">
         <div className="card glass">
           <div className="card-body p-0">
             <h2 className="text-center card-title h2-preview">Preview</h2>
